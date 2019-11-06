462fc0e3370b03459c05fc8f553d73337a03c435
vijay <vijaysimha3103@gmail.com>
2019-11-06 15:15:35 +0530

    reddy

---
+++ vijay.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+	 int r,n,sum,temp;
+	  printf("enter a number:");
+	   scanf("%d",&n);
+	   temp=n;
+	   while(n!=0)
+	   {
+		   r=n%10;
+		   sum=sum+r*r*r;
+		   n=n/10;
+	   }
+	   if(sum==temp)
+		   printf("%d is a armstrong num",temp);
+	   else
+		   printf("%d is  not a armstrong num",temp);
+}
+
